4fe38ab666a89e87e65385a7532c8c64264f02e8
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-06-07 23:16:09 -0500

    Join products and categories with product_category model and schema

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :product_categories
+    has_many :products, through: :product_categories, source: :product_categories_table_foreign_key_to_products_table
 end
