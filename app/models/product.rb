421ba119e68dd9cb1aadfe912d15e7dd90fbe64e
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-06-08 00:08:52 -0500

    Update orderToOrderItem and productToProductVariant associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     has_many :product_categories
-    has_many :categories, through: :product_categories, source: :product_categories_table_foreign_key_to_categories_table
+    has_many :categories, through: :product_categories
+    has_many :variants, class_name: 'ProductVariant'
 end
