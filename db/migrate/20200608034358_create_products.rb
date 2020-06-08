9c1c92c9c5f24b1cfa03f182d032a3dc9d1472d6
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-06-07 22:46:48 -0500

    Add products model and schema

---
+++ db/migrate/20200608034358_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :title, limit: 150, null: false
+      t.text :description
+      t.decimal :price, precision: 15, scale: 2, null: false, default: 0
+
+      t.timestamps
+
+      t.index :title, unique: true
+    end
+  end
+end
