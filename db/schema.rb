00bb05cbeb810be9543a20721b7ad893d45bab11
Omeshwor <omeshworlalshrestha@yahoo.com>
2020-06-09 11:34:22 -0500

    Add checkout feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_045559) do
+ActiveRecord::Schema.define(version: 2020_06_09_162225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_045559) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "token"
+    t.string "status", default: "cart"
   end
 
   create_table "product_categories", force: :cascade do |t|
